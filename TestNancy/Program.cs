865e604d590ac2d1db6503d0242b59d66b13785c
theClueless <walla.ata@gmail.com>
2015-10-17 01:35:27 +0300

    an static html is returning

---
+++ TestNancy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var nancyHost = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:9664"));
+             nancyHost.Start();
+             Console.WriteLine("Web server running...");
+             
+             Console.ReadLine();
+             nancyHost.Stop();
+        }
+    }
+}
---
+++ TestNancy/TestModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNancy
+{
+    using System.Reflection;
+
+    using Nancy;
+    using Nancy.Bootstrapper;
+    using Nancy.Diagnostics.Modules;
+    using Nancy.TinyIoc;
+    using Nancy.ViewEngines;
+
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/"] = p => View["Main.html"];
+        }
+    }
+
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override NancyInternalConfiguration InternalConfiguration
+        {
+            get
+            {
+                return NancyInternalConfiguration.WithOverrides(OnConfigurationBuilder);
+            }
+        }
+
+        void OnConfigurationBuilder(NancyInternalConfiguration x)
+        {
+            x.ViewLocationProvider = typeof(ResourceViewLocationProvider);
+        }
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            ResourceViewLocationProvider.RootNamespaces.Add(
+              Assembly.GetAssembly(typeof(TestModule)), "TestNancy.Views");
+        }
+    }
+}
